2f2212552aa04a4070662826d427b9fa656cd207
Jonathan Lazarte <lazartejonathan10@gmail.com>
2026-08-08 16:04:50 -0300

    feat: please wait message in main route

--- app/page.js
+++ app/page.js
@@ -4,8 +4,9 @@
 export default function HomePage() {
 
 
-  return <>
-    <img style={{ width: "125px"}} src="/riot_offwhite.png"></img>
-  </>
+  return (<div style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
+    <img style={{ width: "125px" }} src="/riot_offwhite.png"></img>
+    <span style={{ color: "white", marginTop: "2rem", textAlign: "center", fontSize: "1.1rem" }}>Connecting to the server, please wait...</span>
+  </div>)
 
 }
