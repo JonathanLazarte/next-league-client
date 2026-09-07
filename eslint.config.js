6afeb86277d67382e137f8e78f2a801407aaa078
Jonathan Lazarte <lazartejonathan10@gmail.com>
2026-09-07 01:25:22 -0300

    style: eslint rule fix

--- eslint.config.js
+++ eslint.config.js
@@ -7,14 +7,18 @@ export default tseslint.config(
   ...tseslint.configs.strictTypeChecked, // Nivel estricto para TypeScript
   ...tseslint.configs.stylisticTypeChecked, // Evalúa consistencia de estilo
   {
+    parser: "@typescript-eslint/parser",
     plugins: {
       '@next/next': nextPlugin,
+      "plugins": ["@typescript-eslint"],
     },
     rules: {
       ...nextPlugin.configs.recommended.rules,
       ...nextPlugin.configs['core-web-vitals'].rules,
       "no-console": "error", // Cambia a 'error' para que no pase ningún console.log
       "@typescript-eslint/no-explicit-any": "error", // Prohibido usar 'any'
+      "no-unused-vars": "off",
+      "@typescript-eslint/no-unused-vars": ["error"]
     },
   }
 );
