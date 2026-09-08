44a7fc88f9c28f14d8d024ddf32b8058aa2bfe4c
Jonathan Lazarte <lazartejonathan10@gmail.com>
2026-09-08 16:02:39 -0300

    ref: ts files migration

--- app/(dashboard)/layout.js
+++
@@ -1,5 +0,0 @@
-import DashboardShell from "@/layout/DashboardShell";
-
-export default function DashboardLayout({ children }) {
-  return <DashboardShell>{children}</DashboardShell>;
-}
---
+++ app/(dashboard)/layout.tsx
@@ -0,0 +1,5 @@
+import DashboardShell from "@/layout/DashboardShell";
+
+export default function DashboardLayout({ children }) {
+  return <DashboardShell>{children}</DashboardShell>;
+}
